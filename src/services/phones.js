cd040df48abcb4b38e8fc21b00579a02307c7161
Anuska86 <anasrm86@gmail.com>
2024-06-26 12:07:36 +0200

    Connect

--- src/services/phones.js
+++ src/services/phones.js
@@ -1,18 +1,29 @@
 import axios from "axios";
-const baseUrl = 'http://localhost:3001/api/phones';
+const baseUrl = 'http://localhost:3001/';
 
-const getAll = () => {
-  const request = axios.get(baseUrl);
+const getAllInfo = () => {
+  const request = axios.get(baseUrl+'api/info');
   return request.then((response) => response.data);
 };
+
+const getAll = () => {
+  const request = axios.get(baseUrl+'api/phones');
+  return request.then((response) => response.data);
+};
+
+const getAllFavourites = () => {
+  const request = axios.get(baseUrl+'api/phones/favourites');
+  return request.then((response) => response.data);
+};
+
 const create = (newObject) => {
-  const request = axios.post(baseUrl, newObject);
+  const request = axios.post(baseUrl+'api/phones', newObject);
   return request.then((response) => response.data);
 };
 
 const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject);
+  const request = axios.put(`${baseUrl}+'api/phones'/${id}`, newObject);
   return request.then((response) => response.data);
 };
 
-export default { getAll, create, update };
+export default { getAll, create, update,getAllInfo,getAllFavourites };
